4a188464906627f4f528b191deb5c6f13f68019e
Ozzie Neher <oz.neher@gmail.com>
2016-02-24 18:40:11 -0600

    Added npm dependencies for ts

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -4,6 +4,7 @@ var sourcemaps = require('gulp-sourcemaps');
 var cssnano = require('gulp-cssnano');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var ts = require('gulp-typescript');
 
 gulp.task('scss', function() {
    return gulp.src('scss/**/*.scss')
@@ -19,9 +20,19 @@ gulp.task('scss', function() {
      .pipe(gulp.dest('dist/'));
 });
 
+gulp.task('typescript', function() {
+  return gulp.src('js/**/*.ts')
+    .pipe(ts({
+			noImplicitAny: true,
+			out: 'bundle.js'
+		}))
+		.pipe(gulp.dest('dist/'));
+});
+
 
 gulp.task('watch', ['scss'], function () {
   gulp.watch('scss/**/*.scss', ['scss']);
+  gulp.watch('js/**/*.ts', ['typescript']);
 });
 
 gulp.task('default', ['watch']);
